77a135d2252cd03a0ae4b2d6759248229542bc9a
D.KOFY <whitebarb.dz@gmail.com>
2020-12-04 16:15:29 +0100

    suppression

--- REACT-APP/my-app/src/testmongodb.js
+++
@@ -1,9 +0,0 @@
-
-const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://root:<password>@database.wdiqg.mongodb.net/<dbname>?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-console.log("DATABASE CREATED");
-  client.close();
-});
