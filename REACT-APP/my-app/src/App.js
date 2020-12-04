98ceaf17c7d220d05ab8d90f7e8992e44bbe56d6
higginal <alec.higgins@etu.univ-grenoble-alpes.fr>
2020-12-04 15:59:12 +0100

    app

--- REACT-APP/my-app/src/App.js
+++ REACT-APP/my-app/src/App.js
@@ -2,17 +2,23 @@ import './App.css';
 import Header from './Header';
 import Accueil from './Accueil';
 import SignIn from './SignIn';
+import Connect from './Conn';
+import Profile from './Profile';
+import ApresConn from './apresConn';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 
 function App() {
   return (
     <div>
-      <Router>
+    <Router>
         <Header /> 
         <Switch>
           <Route path="/" exact component={Accueil}/>
+          <Route path="/apresConn" component={ApresConn}/>
           <Route path="/signIn" component={SignIn}/>
+          <Route path="/Conn" component={Connect}/>
+          <Route path="/Profile" component={Profile}/>
         </Switch>
     </Router>
       
