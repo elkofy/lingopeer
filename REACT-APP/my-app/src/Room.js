992296173aa3a8aa7a6431b0cb7a0dec89337256
Louis <louis.clerc@etu.univ-grenoble-alpes.fr>
2020-12-17 14:34:04 +0100

    test video

--- REACT-APP/my-app/src/Room.js
+++ REACT-APP/my-app/src/Room.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Room.css';
 
-
 const Room = () => {
     return(
-      <h1>Room</h1>
+      <div>
+        <h1>Room</h1>
+        <script src="../../backend/public/scriptRoom.js"></script>
+        <script defer src="https://unpkg.com/peerjs@1.2.0/dist/peerjs.min.js"></script>
+        <script src="/socket.io/socket.io.js" defer></script>
+        <script src="../../backend/public/script.js" defer></script>
+        
+        <div id="video-grid"></div>
+      </div>
+            
     );
 }
 
